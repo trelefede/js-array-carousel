4854c725d61b846501a8580763c666176ac0a3bc
TreleaniFederico <treleani.federico@gmail.com>
2022-09-04 16:45:53 +0200

    add if condition to activeIndex before for-cicle

--- js/script.js
+++ js/script.js
@@ -6,15 +6,49 @@ const arrayImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 // leggo il div container del carosello in html
 const carousel = document.querySelector(".carousel")
 
+let activeIndex = 0;
+
+// ciclo for per aggiungere al div carousel le rispettive immagini con rispettive classi
 for (let index = 0; index < arrayImages.length; index++) {
-    carousel.innerHTML = '<img class="carousel-item active" src="images/' + arrayImages[0] + '" alt="travel1">'
-    const previousBtn = document.getElementById("previous").addEventListener("click", function () {
-        carousel.innerHTML = '<img class="carousel-item active" src="images/' + arrayImages[index--] + '" alt="travel1">'
-    })
-    const nextBtn = document.getElementById("next").addEventListener("click", function () {
-        carousel.innerHTML = '<img class="carousel-item active" src="images/' + arrayImages[index++] + '" alt="travel1">'
-    })
+    const image = arrayImages[index];
+    const classImage = index === activeIndex ? 'carousel-item active' : 'carousel-item';
+    carousel.innerHTML += '<img class="' + classImage + '" src="images/' + image + '" alt="travel' + index + '">'
 }
+
+// leggo bottoni dal'html
+const nextBtn = document.getElementById("next")
+const previousBtn = document.getElementById("previous")
+
+// creo gli eventi alla pressione di next
+nextBtn.addEventListener("click", function () {
+    console.log('next cliccato');
+
+    if (activeIndex < (arrayImages.length - 1)) {
+        activeIndex++;
+
+        for (let index = 0; index < arrayImages.length; index++) {
+            const image = arrayImages[index];
+            const classImage = index === activeIndex ? 'carousel-item active' : 'carousel-item';
+            carousel.innerHTML += '<img class="' + classImage + '" src="images/' + image + '" alt="travel' + index + '">'
+        }
+    }
+
+})
+
+// creo gli eventi alla pressione di previous
+previousBtn.addEventListener("click", function () {
+    console.log('previous cliccato');
+
+    if (activeIndex > 0) {
+        activeIndex--;
+        for (let index = 0; index < arrayImages.length; index++) {
+            const image = arrayImages[index];
+            const classImage = index === activeIndex ? 'carousel-item active' : 'carousel-item';
+            carousel.innerHTML += '<img class="' + classImage + '" src="images/' + image + '" alt="travel' + index + '">'
+        }
+    }
+})
+
 
 
 
