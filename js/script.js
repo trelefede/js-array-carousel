6692c9f794b8fa41a6f7ae3cca3cfbacbf277aef
TreleaniFederico <treleani.federico@gmail.com>
2022-09-04 14:27:10 +0200

    add possible carousel function

--- js/script.js
+++ js/script.js
@@ -7,7 +7,16 @@ const arrayImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
 const carousel = document.querySelector(".carousel")
 
 for (let index = 0; index < arrayImages.length; index++) {
-    carousel.innerHTML += '<img class="carousel-item" src="images/' + arrayImages[index] + '" alt="travel1">'
-    console.log(carousel);
+    carousel.innerHTML = '<img class="carousel-item active" src="images/' + arrayImages[0] + '" alt="travel1">'
+    const previousBtn = document.getElementById("previous").addEventListener("click", function () {
+        carousel.innerHTML = '<img class="carousel-item active" src="images/' + arrayImages[index--] + '" alt="travel1">'
+    })
+    const nextBtn = document.getElementById("next").addEventListener("click", function () {
+        carousel.innerHTML = '<img class="carousel-item active" src="images/' + arrayImages[index++] + '" alt="travel1">'
+    })
 }
+
+
+
+
 
