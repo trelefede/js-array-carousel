8a78c4cb5569da18419d5f5c6ae6705b1eac2080
TreleaniFederico <treleani.federico@gmail.com>
2022-09-04 13:55:53 +0200

    add js cicle for to add images in html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-console.log('js ok')
+console.log('js ok');
 
-// inizializzo le variabili prendendole dall'html
-const 
+// creo array contenente immagini
+const arrayImages = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+
+// leggo il div container del carosello in html
+const carousel = document.querySelector(".carousel")
+
+for (let index = 0; index < arrayImages.length; index++) {
+    carousel.innerHTML += '<img class="carousel-item" src="images/' + arrayImages[index] + '" alt="travel1">'
+    console.log(carousel);
+}
+
