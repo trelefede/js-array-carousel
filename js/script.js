a95da9bb03c3ae01d3e528fb9274641dfd4950f1
TreleaniFederico <treleani.federico@gmail.com>
2022-09-06 16:42:37 +0200

    add js changes to listener next button

--- js/script.js
+++ js/script.js
@@ -23,6 +23,11 @@ const previousBtn = document.getElementById("previous")
 nextBtn.addEventListener("click", function () {
     console.log('next cliccato');
 
+    if (activeIndex === arrayImages.length - 1) {
+        activeIndex = -1;
+    }
+
+    // active index non deve superare mai la lunghezza dell'array
     if (activeIndex < (arrayImages.length - 1)) {
         activeIndex++;
 
@@ -33,20 +38,30 @@ nextBtn.addEventListener("click", function () {
         }
     }
 
+    console.log(activeIndex);
+
 })
 
 // creo gli eventi alla pressione di previous
 previousBtn.addEventListener("click", function () {
     console.log('previous cliccato');
 
+    if (activeIndex === 0) {
+        activeIndex = arrayImages.length;
+    }
+
+    // active index non deve mai andare sotto lo zero
     if (activeIndex > 0) {
         activeIndex--;
+
         for (let index = 0; index < arrayImages.length; index++) {
             const image = arrayImages[index];
             const classImage = index === activeIndex ? 'carousel-item active' : 'carousel-item';
             carousel.innerHTML += '<img class="' + classImage + '" src="images/' + image + '" alt="travel' + index + '">'
         }
     }
+    console.log(activeIndex);
+
 })
 
 
